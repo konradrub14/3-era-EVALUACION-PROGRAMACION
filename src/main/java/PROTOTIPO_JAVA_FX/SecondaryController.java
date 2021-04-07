3c7af872bb3aac08db5032cb666758168eadf892
konra <konra@DESKTOP-54074D1>
2021-04-07 22:53:12 +0200

    TRABAJO ENTORNOS

---
+++ src/main/java/PROTOTIPO_JAVA_FX/SecondaryController.java
@@ -0,0 +1,12 @@
+package PROTOTIPO_JAVA_FX;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
